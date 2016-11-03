ce0cdff0ab60490e5e8f22411f4caaddc44b6bc9
Z.Withrow <zachary.withrow.537@gmail.com>
2016-11-03 17:56:55 -0600

    First day finalize

--- Arrigo1/Arrigo1/Arrigo.Designer.vb
+++ Arrigo1/Arrigo1/Arrigo.Designer.vb
@@ -1,4 +1,4 @@
-<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class Arrigo
     Inherits DevComponents.DotNetBar.Metro.MetroForm
 
@@ -22,19 +22,86 @@ Partial Class Arrigo
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.StyleManager1 = New DevComponents.DotNetBar.StyleManager(Me.components)
+        Me.SideNav1 = New DevComponents.DotNetBar.Controls.SideNav()
+        Me.SideNavItem1 = New DevComponents.DotNetBar.Controls.SideNavItem()
+        Me.Separator1 = New DevComponents.DotNetBar.Separator()
+        Me.SideNavItem2 = New DevComponents.DotNetBar.Controls.SideNavItem()
+        Me.SideNavPanel1 = New DevComponents.DotNetBar.Controls.SideNavPanel()
+        Me.SideNav1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'StyleManager1
+        '
+        Me.StyleManager1.ManagerStyle = DevComponents.DotNetBar.eStyle.Metro
+        Me.StyleManager1.MetroColorParameters = New DevComponents.DotNetBar.Metro.ColorTables.MetroColorGeneratorParameters(System.Drawing.Color.White, System.Drawing.Color.FromArgb(CType(CType(150, Byte), Integer), CType(CType(10, Byte), Integer), CType(CType(10, Byte), Integer)))
+        '
+        'SideNav1
+        '
+        Me.SideNav1.Controls.Add(Me.SideNavPanel1)
+        Me.SideNav1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.SideNav1.Items.AddRange(New DevComponents.DotNetBar.BaseItem() {Me.SideNavItem1, Me.Separator1, Me.SideNavItem2})
+        Me.SideNav1.Location = New System.Drawing.Point(0, 0)
+        Me.SideNav1.Name = "SideNav1"
+        Me.SideNav1.Padding = New System.Windows.Forms.Padding(1)
+        Me.SideNav1.Size = New System.Drawing.Size(1457, 764)
+        Me.SideNav1.TabIndex = 0
+        Me.SideNav1.Text = "SideNav1"
+        '
+        'SideNavItem1
+        '
+        Me.SideNavItem1.IsSystemMenu = True
+        Me.SideNavItem1.Name = "SideNavItem1"
+        Me.SideNavItem1.Symbol = ""
+        Me.SideNavItem1.Text = "Menu"
+        '
+        'Separator1
+        '
+        Me.Separator1.FixedSize = New System.Drawing.Size(3, 1)
+        Me.Separator1.Name = "Separator1"
+        Me.Separator1.Padding.Bottom = 2
+        Me.Separator1.Padding.Left = 6
+        Me.Separator1.Padding.Right = 6
+        Me.Separator1.Padding.Top = 2
+        Me.Separator1.SeparatorOrientation = DevComponents.DotNetBar.eDesignMarkerOrientation.Vertical
+        '
+        'SideNavItem2
+        '
+        Me.SideNavItem2.Checked = True
+        Me.SideNavItem2.Name = "SideNavItem2"
+        Me.SideNavItem2.Panel = Me.SideNavPanel1
+        Me.SideNavItem2.Symbol = ""
+        Me.SideNavItem2.Text = "Home"
+        '
+        'SideNavPanel1
+        '
+        Me.SideNavPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SideNavPanel1.Location = New System.Drawing.Point(78, 36)
+        Me.SideNavPanel1.Name = "SideNavPanel1"
+        Me.SideNavPanel1.Size = New System.Drawing.Size(1374, 727)
+        Me.SideNavPanel1.TabIndex = 2
         '
         'Arrigo
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(653, 436)
+        Me.ClientSize = New System.Drawing.Size(1469, 764)
+        Me.Controls.Add(Me.SideNav1)
         Me.DoubleBuffered = True
         Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.ForeColor = System.Drawing.Color.Black
         Me.Name = "Arrigo"
-        Me.Text = "MetroForm"
+        Me.Text = "Arrigo"
+        Me.SideNav1.ResumeLayout(False)
+        Me.SideNav1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents StyleManager1 As DevComponents.DotNetBar.StyleManager
+    Friend WithEvents SideNav1 As DevComponents.DotNetBar.Controls.SideNav
+    Friend WithEvents SideNavPanel1 As DevComponents.DotNetBar.Controls.SideNavPanel
+    Friend WithEvents SideNavItem1 As DevComponents.DotNetBar.Controls.SideNavItem
+    Friend WithEvents Separator1 As DevComponents.DotNetBar.Separator
+    Friend WithEvents SideNavItem2 As DevComponents.DotNetBar.Controls.SideNavItem
 End Class
